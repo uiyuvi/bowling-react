47a05e07227e2b113959b2cec824e4fa406cd94d
Ranganathan Yuvaraj <I46628@INTNP.SYSNP.SHARED.FORTIS>
2019-04-18 16:56:51 +0200

    test: Init game before each test and Extract rolling to function

--- src/App.test.js
+++ src/App.test.js
@@ -5,6 +5,20 @@ import {configure , shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
+let game;
+function getScore() {
+  return game.instance().score();
+}
+
+function rollMany(rollCount,pin) {
+  for(let i = 0; i < rollCount; i++){
+    game.instance().roll(pin);
+  }
+}
+
+beforeEach(() => {
+  game = shallow(<App />);
+})
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -13,23 +27,15 @@ it('renders without crashing', () => {
 });
 
 it('should handle `Gutter` game', () => {
-  var game = shallow(<App />);
-
-  for(let i = 0; i < 20; i++){
-    game.instance().roll(0);
-  }
+  rollMany(20,0);
   
-  expect(game.instance().score()).toBe(0);
+  expect(getScore()).toBe(0);
 });
 
 it('should return 20 for all 1', () => {  
-  var game = shallow(<App />);
-
-  for(let i = 0; i < 20; i++){
-    game.instance().roll(1);
-  }
+  rollMany(20,1);
   
-  expect(game.instance().score()).toBe(20);
+  expect(getScore()).toBe(20);
 });
 
 
