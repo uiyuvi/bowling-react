36487b2565feaeeb293801ccf7b5ffd1d0b5db56
Ranganathan Yuvaraj <I46628@INTNP.SYSNP.SHARED.FORTIS>
2019-04-18 16:24:50 +0200

    feat: Hanlde Gutter game

--- src/App.js
+++ src/App.js
@@ -2,22 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rolls: [],
+      score: null
+    }
+  };
+
+  roll = (pin) => {
+    let updatedRolls = this.state.rolls.concat(pin);
+    this.setState({rolls: updatedRolls});
+  }  
+
+  score = () => {
+    let score = 0;
+    for(let roll = 0; roll < this.state.rolls.length; roll++){
+      score += this.state.rolls[roll];
+    }
+    this.setState({score: score});
+    return this.state.score;
+  }
+
+  rollPins = () => {
+    for(let i = 0; i < 20; i++){
+      this.roll(0);
+    }
+    this.score();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type ="button" onClick={this.rollPins} value = "Start game"/>
+        <p> Your score is : {this.state.score} </p>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {configure , shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('should handle `Gutter` game', () => {
+  var game = shallow(<App />);
+
+  for(let i = 0; i < 20; i++){
+    game.instance().roll(0);
+  }
+  
+  expect(game.instance().score()).toBe(0);
+});
+
+
