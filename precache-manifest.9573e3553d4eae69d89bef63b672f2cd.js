0ecf420dedd5455666429abb44aba34c8695672a
Ranganathan Yuvaraj <I46628@INTNP.SYSNP.SHARED.FORTIS>
2019-07-09 17:41:32 +0200

    Updates

---
+++ precache-manifest.9573e3553d4eae69d89bef63b672f2cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c0349b83985afa7676f",
+    "url": "/bowling-react/static/js/runtime~main.2c0349b8.js"
+  },
+  {
+    "revision": "b27dcea7b3b9d17b93b3",
+    "url": "/bowling-react/static/js/main.b27dcea7.chunk.js"
+  },
+  {
+    "revision": "604dacc94ff142ca2b4a",
+    "url": "/bowling-react/static/js/1.604dacc9.chunk.js"
+  },
+  {
+    "revision": "b27dcea7b3b9d17b93b3",
+    "url": "/bowling-react/static/css/main.c95d9344.chunk.css"
+  },
+  {
+    "revision": "2a00b061d7d5b63044290105f82574a0",
+    "url": "/bowling-react/index.html"
+  }
+];
